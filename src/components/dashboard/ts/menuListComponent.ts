661ad1b19d2f9c57be0287782dd094b3b7604150
Hossein Pourghadiri <49945753+pourghadiriHossein@users.noreply.github.com>
2023-06-22 15:47:03 +0330

    Update menuListComponent.ts

--- src/components/dashboard/ts/menuListComponent.ts
+++ src/components/dashboard/ts/menuListComponent.ts
@@ -5,7 +5,7 @@ type menuItem = {name: string, route: string, icon: string};
 
 function menu() {
   return [
-    { name: 'Dashboard', route: 'dashboard', icon: mdiHome },
+    { name: 'Dashboard', route: 'dashboardIndex', icon: mdiHome },
     { name: 'My Posts', route: 'myPosts', icon: mdiNote },
     { name: 'All Posts', route: 'allPosts', icon: mdiNoteMultiple },
     { name: 'All Users', route: 'allUsers', icon: mdiAccountGroup },
